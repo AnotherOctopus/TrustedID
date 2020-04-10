23f97bac4faac7aaaf3dfb7e4634ac6d7ec8f74d
AnotherOctopus <li1735@purdue.edu>
2020-04-10 01:28:11 -0700

    g

--- main.go
+++ main.go
@@ -15,15 +15,9 @@ func main() {
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
 	})
 
-	router.Run(":" + port)
+	http.ListenAndServe(":" + port,nil)
 }
